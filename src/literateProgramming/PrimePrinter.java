95ee5171da0474ca42be2b5c9cac1c020fe9eae8
ganzorigo21 <od.ganzorig12@gmail.com>
2019-05-01 16:14:59 -0400

    refractor to create PrimeGenerator an NumberPrinter classes

--- src/literateProgramming/PrimePrinter.java
+++ src/literateProgramming/PrimePrinter.java
@@ -5,9 +5,8 @@ public class PrimePrinter {
     public static void main(String[] args) {
         final int numPrimes = 1000;
         PrimeGenerator primeGenerator = new PrimeGenerator(numPrimes);
-        int[] primes = primeGenerator.generatePrimes();
         NumberPrinter numberPrinter = new NumberPrinter(4, 50);
-        numberPrinter.printNumber(numPrimes, primes);
+        numberPrinter.print(numPrimes, primeGenerator.generate());
     }
 
 }
